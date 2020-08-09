77bcd649bc8ad86bf7c04ad935d55dd1f203878d
jaehyun <wogus7an@gmail.com>
2020-08-09 14:43:12 +0900

    y축 그리기, x축 가변

--- src/search.js
+++ src/search.js
@@ -11,25 +11,39 @@ export const Search = () => {
   }
 
   const searchCountry = async () => {
-    await fetch(`https://api.covid19api.com/dayone/country/${country}/status/confirmed`).then(res => res.json().then(data => {
+    await fetch(`https://api.covid19api.com/dayone/country/${country}`).then(res => res.json().then(data => {
       setCovidData(data)
     }))
   }
 
   const svg = d3.select('svg');
+  const width = 1800;
+  const height = 500;
 
   if (covidData.length > 0) {
     console.log(covidData)
     const timeRange = d3.extent(covidData, function (d) { return new Date(d.Date) });
-    const x = d3.scaleTime().domain(timeRange).range([0, 1600]);
+    const x = d3.scaleTime().domain(timeRange).range([0, covidData * 1800]);
     svg.append('g')
-      .attr('transform', 'translate(' + 32 + ',' + (500 - margin.bottom - margin.top - 32) + ')')
-      .call(d3.axisBottom(x).ticks(d3.timeWeek))
+      .attr('transform', 'translate(' + 60 + ',' + (500 - margin.bottom - margin.top - 32) + ')')
+      .call(d3.axisBottom(x).ticks(d3.timeDay))
 
-    const confirmedRange = d3.extent(covidData, function (d) { return d.Cases });
-    const y = d3.scaleLinear().domain(confirmedRange).range([0, 500]);
+
+    const confirmedRange = d3.extent(covidData, function (d) { return d.Confirmed });
+    const y = d3.scaleLinear().domain(confirmedRange).range([400, 0]);
     svg.append('g')
+      .attr('transform', 'translate(60, 4)')
       .call(d3.axisLeft(y))
+
+    // svg.append('g')
+    //   .selectAll('.bar')
+    //   .data(covidData)
+    //   .enter().append('rect')
+    //   .attr('class', 'bar')
+    //   .attr('x', function (d) { return x(new Date(d.Date)) })
+    //   .attr('y', function (d) { return y(d.Confirmed) })
+    //   .attr('width', 30)
+    //   .attr('height', function (d) { return 400 / d.Confirmed })
   }
 
   return (
@@ -37,7 +51,7 @@ export const Search = () => {
       <h1>Search Country</h1>
       <input type='text' onChange={(e) => changeText(e)} value={country} />
       <button onClick={searchCountry}>Search</button>
-      <svg width='1600' height='500' />
+      <svg width={width} height={height} />
     </>
   )
 }
