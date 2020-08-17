09a488e13c75b79c44d2fd5953c1e8ec75ce89bb
jaehyun <wogus7an@gmail.com>
2020-08-17 12:16:50 +0900

    y축

--- src/search.js
+++ src/search.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as d3 from 'd3';
+import { timeDay } from 'd3';
 
 export const Search = () => {
   const [country, setCountry] = React.useState('');
@@ -11,39 +12,41 @@ export const Search = () => {
   }
 
   const searchCountry = async () => {
-    await fetch(`https://api.covid19api.com/dayone/country/${country}`).then(res => res.json().then(data => {
+    await fetch(`https://api.covid19api.com/total/dayone/country/${country}`).then(res => res.json().then(data => {
       setCovidData(data)
     }))
   }
 
   const svg = d3.select('svg');
-  const width = 1800;
-  const height = 500;
+  const width = covidData.length * 41;
+  const height = 700;
 
   if (covidData.length > 0) {
+    console.log(width)
     console.log(covidData)
     const timeRange = d3.extent(covidData, function (d) { return new Date(d.Date) });
-    const x = d3.scaleTime().domain(timeRange).range([0, covidData * 1800]);
+    const xAxis = d3.scaleTime().domain(timeRange).range([0, covidData.length * 40])
     svg.append('g')
-      .attr('transform', 'translate(' + 60 + ',' + (500 - margin.bottom - margin.top - 32) + ')')
-      .call(d3.axisBottom(x).ticks(d3.timeDay))
-
+      .attr('transform', 'translate(' + 60 + ',' + (700 - margin.bottom - margin.top - 32) + ')')
+      .call(d3.axisBottom(xAxis).ticks(timeDay).tickFormat(d3.timeFormat("%m/%d")))
 
     const confirmedRange = d3.extent(covidData, function (d) { return d.Confirmed });
-    const y = d3.scaleLinear().domain(confirmedRange).range([400, 0]);
+    console.log(confirmedRange)
+    const yAxis = d3.scaleLinear().domain(confirmedRange).range([500, 0]);
     svg.append('g')
-      .attr('transform', 'translate(60, 4)')
-      .call(d3.axisLeft(y))
+      .attr('transform', 'translate(60, 104)')
+      .call(d3.axisLeft(yAxis))
 
-    // svg.append('g')
+    // svg
     //   .selectAll('.bar')
     //   .data(covidData)
     //   .enter().append('rect')
     //   .attr('class', 'bar')
-    //   .attr('x', function (d) { return x(new Date(d.Date)) })
-    //   .attr('y', function (d) { return y(d.Confirmed) })
-    //   .attr('width', 30)
-    //   .attr('height', function (d) { return 400 / d.Confirmed })
+    //   .attr('x', function (d) { return xAxis(new Date(d.Date)) })
+    //   .attr('width', 20)
+    //   .attr('y', function (d, i) { return yAxis(d.Confirmed) })
+    //   .attr('height', function (d, i) { return yAxis(d.Confirmed) * i })
+    //   .attr('transform', 'translate(40, -300)')
   }
 
   return (
@@ -51,7 +54,9 @@ export const Search = () => {
       <h1>Search Country</h1>
       <input type='text' onChange={(e) => changeText(e)} value={country} />
       <button onClick={searchCountry}>Search</button>
-      <svg width={width} height={height} />
+      <div id='container'>
+        <svg id="line-graph" width={width} height={height} style={{ overflow: 'scroll' }} />
+      </div>
     </>
   )
 }
